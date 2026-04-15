4409e83b37289846064cd27357a63c441edddf62
vbechev <victor.bechev@gmail.com>
2026-04-15 22:48:09 +0300

    Add effects prototype

--- entities.py
+++ entities.py
@@ -1,5 +1,6 @@
 from enum import StrEnum
 from random import random
+from typing import Self
 
 
 class StatType(StrEnum):
@@ -10,31 +11,42 @@ class StatType(StrEnum):
 
 
 class Stats:
-    def __init__(self, strength, dexterity, vitality, energy):
+    def __init__(self, strength: int, dexterity: int, vitality: int, energy: int):
         self.strength = strength
         self.dexterity = dexterity
         self.vitality = vitality
         self.energy = energy
 
-    def increase_stat(self, stat, value):
+    def increase_stat(self, stat: StatType, value: int):
         current_value = getattr(self, stat)
         setattr(self, stat, current_value + value)
+
+    def __getitem__(self, index):
+        return [self.strength, self.dexterity, self.vitality, self.energy][index]
+
+    def __add__(self, other):
+        return Stats(*(left + right for left, right in zip(self, other)))
+
+
+class Effect:
+    def __init__(self, *stats):
+        self.stats = stats
 
 
 class Entity:
     _BASE_STATS = (0, 0, 0, 0)
 
-    def __init__(self, name, level):
+    def __init__(self, name: str, level: int) -> None:
         self.name = name
         self.level = level
         self.stats = Stats(*self._BASE_STATS)
         self.current_life = self.max_life
 
-    def increase_stat(self, stat, value):
+    def increase_stat(self, stat: StatType, value: int):
         self.stats.increase_stat(stat, value)
 
     @property
-    def max_life(self):
+    def max_life(self) -> int:
         return self.stats.vitality * 5
 
     @property
@@ -47,8 +59,11 @@ class Entity:
         if random() < hit_chance:
             entity.take_damage(damage)
 
-    def take_damage(self, damage):
+    def take_damage(self, damage: float):
         self.current_life -= damage
+
+    def apply_effect(self, effect):
+        self.stats += effect.stats
 
 
 class Enemy(Entity):
@@ -61,6 +76,3 @@ class Barbarian(Entity):
 
 class Sorceress(Entity):
     _BASE_STATS = (5, 10, 10, 15)
-
-
-ivan = Barbarian("Ivan", 1)
--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+from ast import Attribute
+from unittest.mock import patch
 import unittest
 
 from entities import *
@@ -16,9 +18,30 @@ class TestHero(unittest.TestCase):
             self.hero.increase_stat("strongth", 5)
 
     def test_life(self):
-        self.assertEqual(self.hero.life, 0)
+        self.assertEqual(self.hero.max_life, 0)
         self.hero.increase_stat(StatType.VITALITY, 5)
-        self.assertEqual(self.hero.life, 25)
+        self.assertEqual(self.hero.max_life, 25)
+
+    def test_take_damage(self):
+        self.hero.take_damage(10)
+        self.assertEqual(self.hero.current_life, self.hero.max_life - 10)
+
+    def test_attack(self):
+        enemy = Enemy("Evil Person", 1)
+        with patch("entities.random", return_value=0):
+            enemy.attack(self.hero)
+        self.assertEqual(self.hero.current_life, self.hero.max_life - 10)
+
+    def test_attack_miss(self):
+        enemy = Enemy("Evil Person", 1)
+        with patch("entities.random", return_value=1):
+            enemy.attack(self.hero)
+        self.assertEqual(self.hero.current_life, self.hero.current_life)
+
+    def test_apply_effect(self):
+        self.hero.apply_effect(Effect(10, 0, 0, 0))
+        self.assertEqual(self.hero.stats.strength, 10)
+        self.assertEqual(self.hero.stats.dexterity, 0)
 
 
 if __name__ == "__main__":
